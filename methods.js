d9894705f5b73156a28ae61cc36628fd10372985
farsana nkm <fasrsanankm24@gmail.comgit config --global user.email fasrsanagit config --global user.email fasrsanankm24@gmail.com>
2024-02-26 13:01:03 +0530

    string methods

---
+++ methods.js
@@ -0,0 +1,120 @@
+let a="apple,mango,banana,kiwi,rambu";
+let c=a.length;
+console.log(b);
+
+
+
+
+let d="apple,mango,banana,kiwi,rambu";
+let e=d.charAt(0);
+console.log(e);
+
+
+
+let f="apple,mango,banana,kiwi,rambu";
+let g=f.charCodeAt(0);
+console.log(g);
+
+
+
+let h="apple,mango,banana,kiwi,rambu";
+let i=h.charAt(2);
+console.log(i);
+
+
+
+let j="apple,mango,banana,kiwi,rambu";
+let k=j[0];
+console.log(k);
+
+
+
+let l="apple,mango,banana,kiwi,rambu";
+let m=l.slice(2,7);
+console.log(m);
+
+
+
+let n="apple,mango,banana,kiwi,rambu";
+let o=n.substring(3,7);
+console.log(o);
+
+
+
+let p="apple,mango,banana,kiwi,rambu";
+let q=p.substr(3,8);
+console.log(q);
+
+
+
+
+let r="apple,mango,banana,kiwi,rambu";
+let s=r.toUpperCase();
+console.log(s);
+
+
+
+
+let t="apple,mango,banana,kiwi,rambu";
+let u=t.toLowerCase();
+console.log(u);
+
+
+
+let v="              hi     ";
+let w=v.trim();
+console.log(w);
+
+
+
+let x="              hi     ";
+let y=x.trimStart();
+console.log(y);
+
+
+
+
+let z="              hi     ";
+let aa=z.trimEnd();
+console.log(aa);
+
+
+
+
+let ab="6";
+let ac=ab.padStart(4,"0");
+console.log(ac);
+
+
+
+
+let ad="6";
+let af=ad.padEnd(4,"0");
+console.log(af);
+
+
+
+
+let ag="welcome back";
+let ah=ag.repeat(2);
+console.log(ah);
+
+
+
+
+let ai="hi hellow";
+let aj=ai.replace("hellow","bye");
+console.log(aj);
+
+
+
+let ak="hi welcome back. welcome to the world";
+ak=ak.replaceALll("welcome","come");
+console.log(ak);
+
+
+
+
+let al="a*a*aaaa";
+let am=al.split("*");
+console.log(am);
